20c9c26eae4754ceb36101b61a7a81fa95c147c3
KirillDegtyarev <lirik19@yandex.ru>
2023-03-30 21:40:10 +0700

    first commit

---
+++ ec/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+# Register your models here.
---
+++ ec/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Product
+from django.db.models import Count
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        return render(request, "app/category.html",  locals())
+
