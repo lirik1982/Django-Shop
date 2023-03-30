927a744026a97f211d1453cb4647b698db3d3752
KirillDegtyarev <lirik19@yandex.ru>
2023-03-31 00:00:16 +0700

    new

--- ec/app/views.py
+++ ec/app/views.py
@@ -3,13 +3,35 @@ from django.views import View
 from .models import Product
 from django.db.models import Count
 
-# Create your views here.
+
 def home(request):
     return render(request, 'app/home.html')
+
+
+def about(request):
+    return render(request, 'app/about.html')
+
+
+def contact(request):
+    return render(request, 'app/contact.html')
+
 
 class CategoryView(View):
     def get(self, request, val):
         product = Product.objects.filter(category=val)
-        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        title = Product.objects.filter(category=val).values('title')
         return render(request, "app/category.html",  locals())
+
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, "app/category.html",  locals())
+
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html", locals())
 
